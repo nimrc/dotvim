b595b1c69799c341d2af675eb7fa2f18c62c822d
Terry Wang <tanshengwang@taichi.graphics>
2026-03-08 12:51:01 +0800

    Fix init.sh: use correct repo URL, safer install logic

--- init.sh
+++ init.sh
@@ -1,13 +1,26 @@
 #!/usr/bin/env bash
 
-rm -rf ~/.vim ~/.vimrc
-git clone --depth=1 https://github.com/fyibmsd/dotvim.git ~/.vim
+set -e
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+# Backup existing config if present
+if [ -d "$HOME/.vim" ] || [ -f "$HOME/.vimrc" ]; then
+    echo "Backing up existing vim config..."
+    timestamp=$(date +%Y%m%d%H%M%S)
+    [ -d "$HOME/.vim" ] && mv "$HOME/.vim" "$HOME/.vim.bak.$timestamp"
+    [ -f "$HOME/.vimrc" ] && mv "$HOME/.vimrc" "$HOME/.vimrc.bak.$timestamp"
+fi
+
+echo "Cloning dotvim..."
+git clone --depth=1 https://github.com/nimrc/dotvim.git "$HOME/.vim"
+
+echo "Installing vim-plug..."
+curl -fLo "$HOME/.vim/autoload/plug.vim" --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-cd ~/.vim
-ln -s ~/.vim/.vimrc ~/.vimrc
-vim +PlugInstall +q! +q!
+echo "Symlinking .vimrc..."
+ln -sf "$HOME/.vim/.vimrc" "$HOME/.vimrc"
+
+echo "Installing plugins..."
+vim -E -s +PlugInstall +qall || echo "Plugin installation finished with warnings (likely from first run)"
 
 echo "Install complete!"
